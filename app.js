3c5295126dad918e00643d68bb6781e77c459287
Michael Haraeb <michael.haraeb@aquare.la>
2022-07-23 12:40:16 -0300

    feature: Added about and contact pages

--- app.js
+++ app.js
@@ -2,7 +2,16 @@ const express = require("express");
 
 const bodyParser = require("body-parser");
 
+const blogData = require("./blogData.js");
+const ejs = require("ejs");
+
 const app = express();
+
+const date = new Date();
+
+const blogPosts = blogData(date);
+
+console.log(blogPosts[0].heading);
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -11,7 +20,15 @@ app.use(express.static("public"));
 app.set("view engine", "ejs");
 
 app.get("/", function (req, res) {
-  res.render("pages/blog");
+  let newPost = {
+    heading: "Work from home",
+    date: blogPosts[0].date,
+    paragraph: blogPosts[0].paragraph,
+  };
+
+  blogPosts.push(newPost);
+
+  res.render("pages/blog", { blogPosts: blogPosts });
 });
 
 app.get("/about", function (req, res) {
---
+++ blogData.js
@@ -0,0 +1,28 @@
+module.exports = blogForBlog;
+
+function blogForBlog(dateObj) {
+  const year = dateObj.getFullYear();
+  const month = dateObj.getMonth();
+  const day = dateObj.getDate();
+
+  return (data = [
+    {
+      heading: "Programmers and gamers",
+      date: year + "/" + month + "/" + day,
+      paragraph: `Simply dummy text of the printing and typesetting industry. Lorem Ipsum
+          has been the industry's standard dummy text ever since the 1500s, when an
+          unknown printer took a galley of type and scrambled it to make a type
+          specimen book. It has survived not only five centuries, but also the leap
+          into electronic`,
+    },
+    {
+      heading: "technologies that would change the world!",
+      date: year + "/" + month + "/" + day,
+      paragraph: `Simply dummy text of the printing and typesetting industry. Lorem Ipsum
+          has been the industry's standard dummy text ever since the 1500s, when an
+          unknown printer took a galley of type and scrambled it to make a type
+          specimen book. It has survived not only five centuries, but also the leap
+          into electronic`,
+    },
+  ]);
+}
